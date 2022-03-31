3aaae002f9156a904b146b4ae14ab03fff11252f
Mohd Shahnawaz <32464723+shahnawaz8@users.noreply.github.com>
2022-03-31 21:31:30 +0530

    compelte

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,34 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-
+import styled from 'styled-components'
 function App() {
   const [count, setCount] = useState(0)
+  const Button = styled.button`
+  background: ${props => props.primary ? "#2490fe" : "white"};
+  color: ${props => props.primary ? "white" : props.link ? "bLue" : "black"};
+  font-size: 1em;
+  border:${props=>props.default ? "1px solid grey":props.dashes? "1px dashed grey": "none"};
+  margin: 1em;
+  padding: 10px 10px;
+  border-radius: 2px;
+`;
 
+let divStyle ={
+  "textAlign":"left",
+  "margin-left":'20%',
+  'background':'white',
+  'margin-right':'20%',
+  'margin-top':'100px'
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="App" style={divStyle}>
+            <Button primary>Primary Button</Button>
+            <Button default>Default Button</Button>
+            <Button dashes>Dashed Button</Button>
+            <br/> 
+            <Button>Text Button</Button>
+            <Button link>Link Button</Button>
     </div>
   )
 }
